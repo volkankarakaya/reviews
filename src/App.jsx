c82e6cb22e832bad64c3ed31231c66d69f354876
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-24 15:59:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import people from './data';
+import {FaBeer} from 'react-icons/fa'
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const {id, name, job, image, text} = people[index];
+
+  console.log(name)
+
+  return <div>
+    <h2>Reviews Starter</h2>;
+    <FaBeer className="beer"/>
+  </div>
+};
+export default App;
