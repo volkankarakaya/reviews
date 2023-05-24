af9ed72e43c1dc6f29751ca7517b93a5b4fd6a6d
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-24 16:55:09 +0300

    add previous/next person functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from "react";
 import people from "./data";
-import {
-  FaChevronCircleLeft,
-  FaChevronRight,
-  FaQuoteRight,
-} from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const { id, name, job, image, text } = people[index];
 
-  console.log(name);
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+
+      return checkNumber(newIndex);
+    });
+  };
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return checkNumber(newIndex);
+    });
+  };
 
   return (
     <main>
       <article className="review">
         <div className="img-container">
-          <img src={image} alt={name} className="person-img"/>
+          <img src={image} alt={name} className="person-img" />
           <span className="quote-icon">
             <FaQuoteRight />
           </span>
@@ -24,6 +42,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
